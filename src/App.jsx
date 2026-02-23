78318e2fc66f6320340c14761ac8987a62f88d1a
JSwapnali04 <jswapnali2311@gmail.com>
2026-02-23 10:55:31 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
-
 import Header from "./components/Header";
-import Home from "./components/Home";
-import Products from "./components/Products";
-import CategoryPage from "./components/CategoryPage";
-import Contact from "./components/Contact";
-import SignIn from "./components/SignIn";
+import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
-import HeaderHero from "./components/HeaderHero";
-import Footer from "./components/Footer";
-
+import Home from "./components/Home";
+import Footer from "./components/Footer";   // ✅ added
 
 function App() {
-  const [isSignedIn, setIsSignedIn] = useState(false);
-
   return (
     <BrowserRouter>
-      <Header isSignedIn={isSignedIn} setIsSignedIn={setIsSignedIn} />
+      <Header />
 
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/products" element={<Products />} /> */}
-        <Route path="/category/:name" element={<CategoryPage />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/signin" element={<SignIn setIsSignedIn={setIsSignedIn} />} />
+        <Route path="/login" element={<LogIn />} />
         <Route path="/signup" element={<SignUp />} />
-
       </Routes>
-       <Footer />
-    </BrowserRouter>
 
-    
+      <Footer />   {/* ✅ added */}
+    </BrowserRouter>
   );
 }
 
